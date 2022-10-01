7cd5a9d10737cc3f1ea01dede0bd29397b242bbb
Shreyas220 <shreyas.ny@example.com>
2022-10-01 23:30:25 +0530

    adding final changes
    Signed-off-by: Shreyas220 <shreyas.ny@example.com>

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const form = document.getElementById("form");
+const con = document.getElementById("api");
+
 const origin = document.getElementById("Origin");
 const destination = document.getElementById("destination");
 
@@ -12,7 +14,7 @@ function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
     directionsDisplay.setMap(map);
-    
+
     var onSubmit = function(e) {
         var mapRequest = {
             origin: origin.value,
@@ -21,72 +23,77 @@ function initMap() {
         };
         
         e.preventDefault();
-        calcRoute(directionsService, directionsDisplay, mapRequest);
+        calcAndDisplayRoute(directionsService, directionsDisplay, mapRequest);
       };
+
+    var onSubmitapi = function(e) {
+        console.log("inside")
+        var mapRequest = {
+            origin: origin.value,
+            destination: destination.value,
+            travelMode: 'DRIVING'
+        };
+        
+        e.preventDefault();
+        calcAndDisplayAPI(directionsDisplay,mapRequest);
+      };
+
 
     var options = {
         types: ['(cities)']
     }
     var autocomplete1 = new google.maps.places.Autocomplete(origin, options);
     var autocomplete2 = new google.maps.places.Autocomplete(destination, options);
+    con.addEventListener("submit", onSubmitapi); 
     form.addEventListener("submit", onSubmit)
+    
 
 }
 
-function calcRoute(directionsService, directionsDisplay, mapRequest) {
+//function to calculate 
+async function calcAndDisplayRoute(directionsService, directionsDisplay, mapRequest) {
     directionsService.route(mapRequest, function (result, status) {
         if (status == google.maps.DirectionsStatus.OK) {
 
             //Get distance and time
             const output = document.querySelector('#output');
             output.innerHTML = `<div class="resultcontainer" >Driving distance: `+ result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>";
-
+            console.log(result)
             //display route
             directionsDisplay.setDirections(result);
         } else {
             //delete route from map
-            directionsDisplay.setDirections({ routes: [] });
-            //center map in London
+            
             //show error message
             output.innerHTML = ` <div class="noresultcontainer" style="color:white";> Could not retrieve driving distance.</div> `;
         }
     });
 }
 
-//create autocomplete objects for all inputs
-
-
-window.initMap = initMap;
-
-/* async e => {
-    console.log(e);
-    e.preventDefault();
-    const originAddress = origin.value;
-    const destinationAddress = destination.value;
-
-    fetch("http://localhost:8080/getD", {
+//function to get disrance and duration from api 
+async function calcAndDisplayAPI(directionsDisplay,mapRequest){
+    await fetch("http://goserver.gpejavf3ccgpf0c6.eastasia.azurecontainer.io:8080/getdirection", {
         method: 'POST',
         body: JSON.stringify({
-            'origin': originAddress,
-            'destination': destinationAddress
+            'origin': mapRequest.origin,
+            'destination': mapRequest.destination
         })
     })
     .then(response => {
         return response.json();
     })
     .then(data => {
-        container.innerHTML = `
-        <p>Origin: ${data.origin}</p>
-        <p>Destination: ${data.destination}</p>
-        <p>Distance: ${data.distance}</p>
-        `;
+        directionsDisplay.setDirections({ routes: [] });
+        console.log(data)
+        output.innerHTML = `<div class="resultcontainer" >Driving distance: `+ data.distance + ".<br />Duration: " + data.duration + ".</div>";
+
     })
     .catch(errResData => {
-        const error = new Error('Something went wrong!', errResData);
-        error.data = errResData;
-        throw error;
+        directionsDisplay.setDirections({ routes: [] });
+        console.log(errResData)
+        output.innerHTML = ` <div class="noresultcontainer" style="color:white";> Could not retrieve driving distance.</div> `;
     });
+}
 
-
-} */
+window.initMap = initMap;
 
